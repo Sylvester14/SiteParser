fcb4be211b276b9391f775845dfb2b8e591ded9d
Sylvester14 <bratishkashow378@gmail.com>
2025-08-07 21:37:35 +0300

    feat(parsers): добавить парсер товаров
    Реализован парсинг товара для свойств: url, url_image.
    Добавлены тестовые варианты сохранения: text, csv.

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
+import requests as r
+from bs4 import BeautifulSoup
+from parsers import ProductParser
+import csv
+
+products_file_path = "files/products.csv"
+page_template = "?page="
+domain = "https://akson.ru"
+url = "https://akson.ru/kostroma/c/kraski/"
+
+headers = {
+    "Accept": "text/html",
+    "User-Agent": "Mozilla/5.0"
+}
+
 def main():
-    print("Hello from site-parser!")
+    req = r.get(url + page_template + "1", headers)
+    src = req.text
+    
+    data = parse(src)
+    save_to_csv(["url", "url_image"], data, products_file_path)
+        
+        
+def parse(text: str) -> list[dict]:
+    soup = BeautifulSoup(text, "lxml")
+    
+    result = []
+    links = soup.find_all("a", class_="body-m-regular line-clamp-3 text-headline hover:text-theme-primary")
+    
+    for link in links:
+        href = domain + link.get("href")
+        result.append(ProductParser(url=href).parse())
+        
+    return result
+        
+
+def save_to_csv(columns: list[str], data: list[dict], file_path: str):
+    with open(file_path, "w", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=columns, lineterminator="\n")
+        
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def save(text: str, file_path: str):
+    with open(file_path, mode="w", encoding="utf-8") as file:
+        file.write(text)
+
+def save_lines(text_lines: list[str], file_path: str):
+    with open(file_path, mode="w", encoding="utf-8") as file:
+        for line in text_lines:
+            file.write(line + "\n")
+
 
 
 if __name__ == "__main__":
---
+++ parsers/parser.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+import requests as req
+from bs4 import BeautifulSoup
+
+HEADERS = {
+    "Accept": "text/html",
+    "User-Agent": "Mozilla/5.0"
+}
+
+class Parser(ABC):
+    soup: BeautifulSoup
+    url: str
+    
+    def __init__(self, url: str):
+        self.url = url
+        text = req.get(url, HEADERS).text
+        self.soup = BeautifulSoup(text, "lxml")
+    
+    @abstractmethod
+    def parse(self, url: str) -> dict:
+        pass
